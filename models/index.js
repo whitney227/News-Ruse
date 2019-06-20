e2a15031f8a5ee654397b928ff6158f7a94c746e
whitney227 <whitneylcrawford@gmail.com>
2019-06-20 00:48:28 -0400

    installed morgan and set requires for dependencies

---
+++ models/index.js
@@ -0,0 +1,9 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+// ****************************************
+// add all the models here--see example below
+//Article: require("./Article"),
+//Note: require("./Note")
+// ****************************************
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+// require dependencies
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+// TODO
+// ****************************
+// Connect to Mongo DB
+
+// Routes
