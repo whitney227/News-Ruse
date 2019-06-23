01fa547f82567d8c9faf03f61fca1d95b0fe239d
whitney227 <whitneylcrawford@gmail.com>
2019-06-23 12:30:46 -0400

    data scrapped and saved to an array

--- server.js
+++ server.js
@@ -29,10 +29,74 @@ app.use(express.static("public"));
 // ****************************
 
 // Routes
-app.get("/scrape", function(req, res) {
-    axios.get("https://www.huffpost.com/").then(function(response){
-        var $ = cheerio.load(response.data);
 
+// A GET route for scraping the Daily Skimm website
+app.get("/scrape", function (req, res) {
+    // First, use axios to grab the body of the HTML 
+    axios.get("https://theskimm.com/news/daily-skimm").then(function(response){
+        var $ = cheerio.load(response.data);
         
-    })
-})
+        // An empty array to save the data that will get scraped
+        var results = [];
+        
+        // Use cheerio to find the title, link, and summary of each article
+        $("a.card").each(function(i, element) {
+            var title = $(element).children("span").text();
+            var link = $(element).attr("href");
+            var summary = $(element).children("p").text();
+            
+            // save as an object that gets pushed to the results array
+            results.push({
+                title: title,
+                link: link,
+                summary, summary
+            });
+
+            // Create a new Article using the results array built from scraping
+            db.Article.create(results)
+                .then(function(dbArticle) {
+                    //  view the added results in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occured, log it
+                    console.log(err);
+                });
+        });
+        // Send a message to the client
+        res.send("Scrape Complete")
+    });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function(dbArticle) {
+            // if able to find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            // if an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// TODO
+// ********************************************
+// Route for grabbing a specific Article by id
+// Then populate it with it's note
+// ********************************************
+
+// TODO
+// ******************************************************
+// Route for saving/updating an Aritcle's associated Note
+// ******************************************************
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
+
+
+
