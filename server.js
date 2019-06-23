7e964bd03fe1bd8ea5f87e6bc0c74b96c7e10960
whitney227 <whitneylcrawford@gmail.com>
2019-06-23 16:08:29 -0400

    added routes for populating comments to an article id and updating

--- server.js
+++ server.js
@@ -79,17 +79,43 @@ app.get("/articles", function (req, res) {
         });
 });
 
-// TODO
-// ********************************************
 // Route for grabbing a specific Article by id
 // Then populate it with it's comment
-// ********************************************
+app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the comments associated with it
+      .populate("comment")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
-// TODO
-// ******************************************************
 // Route for saving/updating an Aritcle's associated Comment
-// ******************************************************
-
+app.post("/articles/:id", function(req, res) {
+    // Create a new comment and pass the req.body to the entry
+    db.Comment.create(req.body)
+      .then(function(dbComment) {
+        // If a Comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Comment
+        // { new: true } tells the query that we want it to return the updated Comment
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
 // Start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
