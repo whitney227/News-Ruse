22f2f7290507e290b3f258e5526e75294252e577
whitney227 <whitneylcrawford@gmail.com>
2019-07-07 19:55:20 -0400

    local connection working again

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // require dependencies
 var express = require("express");
-var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var exphbs= require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require('./models');
+var mongoose = require("mongoose");
 
 var PORT = 3000;
 // Require routes and use
@@ -20,40 +20,40 @@ app.use("/", routes);
 
 // A GET route for scraping the Daily Skimm website
 app.get("/scrape", function (req, res) {
-  // First, use axios to grab the body of the HTML 
-  axios.get("https://theskimm.com/news/daily-skimm").then(function(response){
-      var $ = cheerio.load(response.data);
-      
-      // An empty array to save the data that will get scraped
-      var results = [];
-      
-      // Use cheerio to find the title, link, and summary of each article
-      $("a.card").each(function(i, element) {
-          var title = $(element).children("span").text();
-          var link = "https://theskimm.com" + $(element).attr("href");
-          var summary = $(element).children("p").text();
-          
-          // save as an object that gets pushed to the results array
-          results.push({
-              title: title,
-              link: link,
-              summary, summary
-          });
-
-          // Create a new Article using the results array built from scraping
-          db.Article.create(results)
-              .then(function(dbArticle) {
-                  //  view the added results in the console
-                  console.log(dbArticle);
-              })
-              .catch(function(err) {
-                  // If an error occured, log it
-                  console.log(err);
-              });
-      });
-      // Send a message to the client
-      res.send("Scrape Complete")
-  });
+    // First, use axios to grab the body of the HTML 
+    axios.get("https://theskimm.com/news/daily-skimm").then(function(response){
+        var $ = cheerio.load(response.data);
+        
+        // An empty array to save the data that will get scraped
+        var results = [];
+        
+        // Use cheerio to find the title, link, and summary of each article
+        $("a.card").each(function(i, element) {
+            var title = $(element).children("span").text();
+            var link = "https://theskimm.com" + $(element).attr("href");
+            var summary = $(element).children("p").text();
+            
+            // save as an object that gets pushed to the results array
+            results.push({
+                title: title,
+                link: link,
+                summary, summary
+            });
+            
+            // Create a new Article using the results array built from scraping
+            db.Article.create(results)
+            .then(function(dbArticle) {
+                //  view the added results in the console
+                console.log(dbArticle);
+            })
+            .catch(function(err) {
+                // If an error occured, log it
+                console.log(err);
+            });
+        });
+        // Send a message to the client
+        res.send("Scrape Complete")
+    });
 });
 
 // Make public a static folder
@@ -69,12 +69,13 @@ app.use(logger("dev"));
 // use bodyParser in our app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 // database connection
-var MONGODB_URI ="mongodb://user1:password1@ds129484.mlab.com:29484/heroku_ds085w99" || "mongodb://localhost:27017/newsdb";
-mongoose.connection(MONGODB_URI);
+mongoose.connect('mongodb://localhost:27017/newsdb', {useNewUrlParser: true});
+// "mongodb://user1:password1@ds129484.mlab.com:29484/heroku_ds085w99",
+
 
 // Start the server
 app.listen(PORT, function() {
